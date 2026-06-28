fbdd2bfacdd83f6b2d3abd17ef6af60bcd7217a5
Neeraj Ojha <nojha1855@gmail.com>
2026-06-28 13:30:25 +0530

    feat: phase2 integration done

--- backend/mcp_server.py
+++ backend/mcp_server.py
@@ -1,16 +1,15 @@
-from mcp import FastMCP
+from mcp.server.fastmcp import FastMCP
 from services.weather import get_current_weather
 from services.accommodation import get_accommodations
 
 mcp = FastMCP("WranglerTools")
 
-@mcp.tools()
+@mcp.tool()
 def fetch_weather(city: str) -> str:
     data = get_current_weather(city)
-
     return str(data)
 
-@mcp.tools()
+@mcp.tool()
 def fetch_hotels(city: str, checkin: str, checkout: str) -> str:
     data = get_accommodations(city, checkin, checkout)
     return str(data)
